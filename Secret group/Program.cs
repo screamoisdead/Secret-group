ea9728652d673ba4677d8a807d0e0f92f2901b70
Jonna Wiberg <jonna.wiberg90@gmail.com>
2020-12-03 16:14:47 +0100

    Add project files.

---
+++ Secret group/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secret_group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
